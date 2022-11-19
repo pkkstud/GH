f563e53d116ec4c00ce8247cfa2f0b25e452dd89
pkkstud <85131562+pkkstud@users.noreply.github.com>
2022-11-19 21:15:11 +0100

    Delete account.js

--- account.js
+++
@@ -1,9 +0,0 @@
-let cart = JSON.parse(sessionStorage.getItem("storeCart")) || [];
-
-function sumOfCart() {
-  let cartSum = document.getElementById("cartsum");
-  let sum = cart.map((data) => data.item).reduce((x, y) => x + y, 0);
-  cartSum.innerHTML = sum;
-}
-
-sumOfCart();
